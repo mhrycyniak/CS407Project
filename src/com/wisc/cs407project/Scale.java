afbc912d4c2abff47ad0e528aac208039b4e4be8
Michael <hrycyniak@wisc.edu>
2014-05-10 20:24:39 -0500

    Remove unused files.
    Everything appears to still work.

--- src/com/wisc/cs407project/Scale.java
+++ src/com/wisc/cs407project/Scale.java
@@ -32,25 +32,21 @@ public class Scale extends Activity {
 		// Create tabs and give them titles
 		ActionBar.Tab walkTab = actionbar.newTab().setText(getString(R.string.walk_path));
 		ActionBar.Tab recordTab = actionbar.newTab().setText(getString(R.string.create_path));
-		// ActionBar.Tab pathTab = actionbar.newTab().setText(getString(R.string.path_settings));
 		ActionBar.Tab scaleTab = actionbar.newTab().setText(getString(R.string.scale_settings));
 
 		// Create fragments
 		WalkFragment walkFragment = new WalkFragment();
 		RecordFragment recordFragment = new RecordFragment();
-		//PathFragment pathFragment = new PathFragment();
 		ScaleExplorerFragment scaleFragment = new ScaleExplorerFragment();
 
 		// Tab listener
 		walkTab.setTabListener(new TabsListener(walkFragment));
 		recordTab.setTabListener(new TabsListener(recordFragment));
-		// pathTab.setTabListener(new TabsListener(pathFragment));
 		scaleTab.setTabListener(new TabsListener(scaleFragment));
 
 		// Add tabs to ActionBar
 		actionbar.addTab(walkTab);
 		actionbar.addTab(recordTab);
-		// actionbar.addTab(pathTab);
 		actionbar.addTab(scaleTab);
 		if (!appDirectorySetup()) {
 			//TODO no storage access, possibly post warning message
